dd39c81d3e8522693235fd97ae0ab21d8d35b0d2
pH2O4 <ph2oalencar@gmail.com>
2022-03-03 00:04:33 -0300

    --- implementing validation ---

--- crud/src/componentes/cadastro.jsx
+++ crud/src/componentes/cadastro.jsx
@@ -2,10 +2,15 @@ import React, { useState, Component } from "react";
 import "./cadastro.css"
 import {Form, Button} from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery'
 
 const Login = () =>{
 const [values, setValues] = useState();
 const ChangingValue = (value) => {
+let senha = $("[name=Senha]").val()
+let senhaV = $("[name=SenhaV]").val()
+if9
+
   setValues((prevValue) => ({
     ...prevValue,
     [value.target.name]: value.target.value,
@@ -13,27 +18,28 @@ const ChangingValue = (value) => {
 };
 
 const ClickButtonS = () =>{
-console.log(values);
 }
-
 return(
 <div className="cadastro">
   <Form className="form">
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Nome Completo</Form.Label>
-    <Form.Control onChange={ChangingValue} name="nome" type="name" placeholder="Ensira seu nome"/>
+    <Form.Control onChange={ChangingValue} name="NomeC" type="text" placeholder="Ensira seu nome"/>
     <Form.Label>Email address</Form.Label>
-    <Form.Control onChange={ChangingValue} name="email" type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
+    <Form.Control onChange={ChangingValue} name="Email" type="Email" placeholder="Enter email" />
+    <Form.Text className="text-mutedd">
      lembre-se de nao compartilhar suas informações de login
     </Form.Text>
   </Form.Group>
 
   <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
-    <Form.Control onChange={ChangingValue} name="senha" type="password" placeholder="Password" />
+    <Form.Control  onChange={ChangingValue} name="Senha" type="password" placeholder="Password" />
     <Form.Label>Ensira a senha novamente</Form.Label>
-    <Form.Control onChange={ChangingValue} name="senhaVe" type="passwordRepeat" placeholder="Password" />
+    <Form.Control  onChange={ChangingValue} name="SenhaV" type="password" placeholder="Password" />
+    <Form.Text className="text-muted">
+    
+    </Form.Text>
   </Form.Group>
   <Button variant="primary" onClick={() => ClickButtonS()} type="submit">
     Submit
